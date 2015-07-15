319e14ad935eb81a69c462d87296c393119bdb6e
Gavin Laking <gavinlaking@gmail.com>
2015-07-15 22:17:54 +0100

    Add padding helper for content.

--- lib/vedeu/templating/helpers.rb
+++ lib/vedeu/templating/helpers.rb
@@ -41,6 +41,49 @@ module Vedeu
         define_stream(style: value, &block)
       end
 
+      # Adds the number of lines or characters provided for each dimension as
+      # padding to the content defined in the given block.
+      #
+      # @example
+      #   {{ padding(1, 2, 1, 2) { 'padded!' } }}
+      #
+      # @param top [Fixnum] Add lines of padding to the top.
+      # @param right [Fixnum] Add characters of padding to the right.
+      # @param bottom [Fixnum] Add lines of padding to the bottom.
+      # @param left [Fixnum] Add characters of padding to the left.
+      # @return [Array<void>]
+      # @raise [InvalidSyntax] The required block was not given.
+      def padding(top = 0, right = 0, bottom = 0, left = 0, &block)
+        out = []
+
+        top.times do |top_lines|
+          out << Vedeu::Line.build(value: ' ')
+        end
+
+        out << define_stream(value: (left * ' ')) if left > 0
+
+        out << define_stream({}, &block)
+
+        out << define_stream(value: (right * ' ')) if right > 0
+
+        bottom.times do |bottom_lines|
+          out << Vedeu::Line.build(value: ' ')
+        end
+
+        out
+      end
+
+      private
+
+      # @see Vedeu::Templating::Helpers#colours
+      def define_colour(attributes = {})
+        attributes.delete_if do |k, v|
+          [:background, :foreground].include?(k) == false || v.nil? || v.empty?
+        end
+
+        Vedeu::Colour.new(attributes)
+      end
+
       private
 
       # @see Vedeu::Templating::Helpers#colour
